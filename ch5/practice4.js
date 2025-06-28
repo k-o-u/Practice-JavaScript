07a22ca63d02c9b6b0bd3e228b7de3cf76e5ae41
k-o-u <ktwmekcm@gmail.com>
2025-06-28 12:29:40 +0900

    数値操作

--- ch5/practice4.js
+++ ch5/practice4.js
@@ -38,3 +38,48 @@ let str4 = '  hello ';
 console.log(`${str4.trim()}`); //hello
 console.log(`${str4.trimStart()}`); //hello_
 console.log(`${str4.trimEnd()}`); //__hello
+
+//置き換え
+console.log(str3.replace('bc', 'XX')); //最初の一つだけ
+console.log(str3.replaceAll('bc', 'XX')); //全て
+
+//分割
+let str5 = 'abcXXdefXXghiXX';
+console.log(str5.split('XX')); //['abc', 'def', 'ghi', '']
+console.log(str5.split('XX', 2)); //['abc', 'def']
+
+//桁を揃える
+let str6 = '98.765';
+console.log(str6.padStart(10)); //____98.765
+console.log(str6.padStart(10, '0')); //000098.765
+console.log(str6.padEnd(10, '0')); //98.7650000
+
+//繰り返し
+console.log(str6.repeat(3)); //98.76598.76598.765
+
+//数値の形式変換
+let num = 987.6543;
+let format = Intl.NumberFormat(
+    'ja-JP',
+    {
+        style: 'currency',
+        currency: 'JPY',
+        currencyDisplay: 'symbol'
+    }
+);
+console.log(format.format(num));  // ¥988
+
+
+// 文字列⇨数値
+let n = '999aaa';
+console.log(Number(n)); //NaN
+console.log(Number.parseFloat(n));  // 999  先頭から連続している数値のみ
+console.log(Number.parseInt(n));  // 999
+console.log(Number.parseFloat('aaa999')); // 先頭が数値じゃないのでNaN
+
+let d = new Date();
+console.log(Number(d)); // 経過ミリ秒に換算した値
+console.log(Number.parseFloat(d)); //NaN parseIntも
+
+
+// 数学の演算機能はMathオブジェクト
